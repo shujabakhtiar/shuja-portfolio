2aa8d6a86ab5ef9f148102b76a25d2317b4b302f
Shuja Bakhtiar <shujabaktiar@gmail.com>
2025-12-13 22:53:36 +0000

    initial load animations

--- src/components/sections/HeroSection.vue
+++ src/components/sections/HeroSection.vue
@@ -1,7 +1,8 @@
 <template>
  <div 
-    class="w-screen h-screen hero-theme overflow-hidden transition-transform duration-700 ease-out"
+    class="w-screen h-screen hero-theme overflow-hidden transition-all duration-700 ease-out"
     :class="isMounted ? 'translate-y-0' : 'translate-y-full'"
+    :style="{ borderRadius: isMounted ? '0' : '50% 50% 0 0 / 100% 100% 0 0' }"
  >
     <div class="w-full h-full flex flex-col items-center justify-center px-5">
       <div class="mb-12 text-center">
